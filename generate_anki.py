d241e06addb7ba993bb0c8dc5126d9c281e24b35
Leo Gaskin <leo.gaskin@le0.gs>
2023-11-18 17:34:22 +0100

    Add PubChem source support

--- generate_anki.py
+++ generate_anki.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import requests
+
 from anki_model import DeckSet, MoleculeNote, Package
 from chembl_webresource_client.new_client import new_client
 from pathlib import Path
@@ -15,9 +17,30 @@ class Row(NamedTuple):
     classification: str
     name: str
     chemblid: str
+    pubchemid: str
 
 
-def write_required_files(dir: Path, chemblid: str):
+def write_files_for_pubchem(dir: Path, pubchemid: str, chemblid: str):
+    rsp = requests.get(
+        f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/{pubchemid}/sdf?record_type=3d"
+    )
+    if rsp.status_code != 200:
+        return
+
+    with dir.joinpath(f"{chemblid}_3D.mol").open("+w") as f:
+        mol = MolFromMolBlock(rsp.text, removeHs=False)
+        f.write(MolToMolBlock(mol))
+
+    with PyMOL() as p:
+        p.cmd.load(dir.joinpath(f"{chemblid}_3D.mol"))
+        p.cmd.color("gray", "(symbol C)")
+        p.cmd.show_as("sticks")
+        p.cmd.orient()
+        p.cmd.zoom(complete=1)
+        p.cmd.png(dir.joinpath(f"{chemblid}.png").as_posix(), 1000, 800, dpi=150, ray=1)
+
+
+def write_files_for_chembl(dir: Path, chemblid: str):
     molecule = new_client.molecule.get(chemblid)  # type:ignore
     if not molecule["molecule_structures"]:
         tqdm.write(f"skipping {chemblid}")
@@ -62,10 +85,13 @@ for sheet_name, sheet in sheets.items():
     for row in tqdm(list(sheet.itertuples()), desc=sheet_name):  # type: ignore
         row: Row
 
-        if pd.isnull(row.chemblid):
+        if pd.notnull(row.pubchemid) and pd.notnull(row.chemblid):
+            write_files_for_pubchem(data_dir, row.pubchemid, row.chemblid)
+        elif pd.notnull(row.pubchemid):
+            write_files_for_chembl(data_dir, row.chemblid)
+        else:
             continue
 
-        write_required_files(data_dir, row.chemblid)
         note = MoleculeNote(name=row.name, chemblid=row.chemblid)
         decks.add_note(
             row.classification if pd.notnull(row.classification) else "Other", note
