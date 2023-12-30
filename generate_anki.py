173b9f1eb434253c865b9aea37d0236c19a8ccb4
Leo Gaskin <leo.gaskin@le0.gs>
2023-12-30 21:16:33 +0100

    Move molecule depiction code to separate file

--- generate_anki.py
+++ generate_anki.py
@@ -1,131 +1,23 @@
 #!/usr/bin/env python
 
 from pathlib import Path
-from typing import Literal
 
 import numpy as np
 import pandas as pd
-import requests
-from pymol2 import PyMOL
-from rdkit.Chem.Draw import MolToImage
-from rdkit.Chem.rdchem import Mol
-from rdkit.Chem.rdDistGeom import EmbedMolecule
-from rdkit.Chem.rdForceFieldHelpers import MMFFOptimizeMolecule
-from rdkit.Chem.rdmolfiles import MolFromMolBlock, MolToMolBlock
-from rdkit.Chem.rdmolops import AddHs
-from rdkit.rdBase import BlockLogs
 from tqdm import tqdm
 
 from anki_model import DeckNumberer, DeckSet, MoleculeNote, Package
+from molecule_depict import MoleculeDepictor
 from spreadsheet_model import Row, SpreadsheetModel
 
-
-def optimize_mol(mol: Mol) -> Mol:
-    try:
-        with BlockLogs():
-            AddHs(mol)
-            EmbedMolecule(mol)
-            MMFFOptimizeMolecule(mol)
-    except BaseException:
-        pass
-
-    return mol
-
-
-def fetch_chembl(id, kind: Literal["2d", "3d"]) -> Mol:
-    rsp = requests.get(
-        f"https://www.ebi.ac.uk/chembl/api/data/molecule/{id}?format=sdf",
-    )
-    if rsp.status_code != 200:
-        raise Exception(f"could not fetch from ChEMBL: '{row.chemblid}'")
-
-    mol = MolFromMolBlock(rsp.text, removeHs=True)
-    mol.SetProp("_Name", row.chemblid)
-
-    if kind == "3d":
-        return optimize_mol(mol)
-
-    return mol
-
-
-def fetch_pubchem(id, kind: Literal["2d", "3d"]) -> Mol:
-    rsp = requests.get(
-        f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/{row.pubchemid}/sdf?record_type={kind}"
-    )
-    if rsp.status_code != 200:
-        if kind == "3d":
-            mol = fetch_pubchem(id, kind="2d")
-            return optimize_mol(mol)
-        else:
-            raise Exception(f"could not fetch from PubChem: '{row.pubchemid}'")
-
-    mol = MolFromMolBlock(rsp.text, removeHs=True)
-    mol.SetProp("_Name", row.pubchemid)
-
-    return mol
-
-
-def fetch_mol(row: Row, kind: Literal["2d", "3d"]) -> Mol:
-    if row.pubchemid:
-        return fetch_pubchem(row.pubchemid, kind)
-
-    if row.chemblid:
-        return fetch_chembl(row.chemblid, kind)
-
-    raise Exception(f"no valid ID present for: {row.name}")
-
-
-def files_exist(dir: Path, row: Row):
-    result = {}
-
-    for id in [row.chemblid, row.pubchemid]:
-        if not id:
-            continue
-
-        path3d = dir.joinpath(id + "_3D.png")
-        if path3d.exists():
-            result["3d"] = path3d.name
-
-        path2d = dir.joinpath(id + "_2D.png")
-        if path2d.exists():
-            result["2d"] = path2d.name
-
-    return result if len(result) == 2 else None
-
-
-def write_files(dir: Path, row: Row):
-    mol3d = fetch_mol(row, kind="3d")
-    pathMol3d = dir.joinpath(f"{mol3d.GetProp('_Name')}_3D.mol")
-    path3d = dir.joinpath(f"{mol3d.GetProp('_Name')}_3D.png")
-
-    with pathMol3d.open("+w") as f:
-        f.write(MolToMolBlock(mol3d))
-
-    with PyMOL() as p:
-        p.cmd.load(pathMol3d)
-        p.cmd.color("gray", "(symbol C)")
-        p.cmd.show_as("sticks, bonded")
-        p.cmd.show_as("nb_spheres, not bonded")
-        p.cmd.hide("(symbol H)")
-        p.cmd.orient()
-        p.cmd.zoom(complete=1)
-        p.cmd.png(path3d.as_posix(), 1000, 800, dpi=150, ray=1)
-
-    mol2d = fetch_mol(row, kind="2d")
-    path2d = dir.joinpath(f"{mol2d.GetProp('_Name')}_2D.png")
-
-    MolToImage(mol2d).save(path2d)
-
-    return {"2d": path2d.name, "3d": path3d.name}
-
-
 upstream_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQgbPmrlTty5Q2luk79OigcbyWyQXAQR4xMpxNWJYHwMPpZvGjhBN7wd88vgAyWGMyzwIedvpR4iiNO/pub?output=xlsx"
-data_dir = Path("data")
+data_dir = Path("data_test")
 data_dir.mkdir(exist_ok=True, parents=True)
 
 sheets = pd.read_excel(upstream_url, sheet_name=None, dtype=str, keep_default_na=False)
 decks = DeckSet("B15 Pharmazeutische Chemie")
 numberer = DeckNumberer()
+depictor = MoleculeDepictor(data_dir)
 
 for sheet_name, sheet in sheets.items():
     if len(sheet_name) < 1 or sheet_name[0] != "#":
@@ -142,16 +34,15 @@ for sheet_name, sheet in sheets.items():
         if row.skip:
             continue
 
-        if not (files := files_exist(data_dir, row)):
-            files = write_files(data_dir, row)
+        output = depictor.depict(row.pubchemid or row.chemblid)
 
         note = MoleculeNote(
             name=row.name,
             taxonomy=row.classification.split("::"),
             chemblid=f"{row.chemblid}",
             pubchemid=f"{row.pubchemid}",
-            file_2d=files["2d"],
-            file_3d=files["3d"],
+            file_2d=output.image2d,
+            file_3d=output.image3d,
         )
 
         decks.add_note(numberer.number(row.classification), note)
---
+++ molecule_depict.py
@@ -0,0 +1,169 @@
+from dataclasses import dataclass
+from os import PathLike
+from pathlib import Path
+from typing import Literal
+
+import requests
+from pymol2 import PyMOL
+from rdkit.Chem.Draw import MolToImage
+from rdkit.Chem.rdchem import Mol
+from rdkit.Chem.rdDepictor import (
+    ConstrainedDepictionParams,
+    GenerateDepictionMatching2DStructure,
+)
+from rdkit.Chem.rdDistGeom import EmbedMolecule
+from rdkit.Chem.rdForceFieldHelpers import MMFFOptimizeMolecule
+from rdkit.Chem.rdmolfiles import MolFromMolBlock, MolToMolBlock
+from rdkit.Chem.rdmolops import AddHs
+from rdkit.rdBase import BlockLogs
+from psico.mcsalign import mcsalign
+
+
+@dataclass
+class MoleculeExport:
+    image2d: str
+    image3d: str
+
+
+class MoleculeDepictor:
+    def __init__(self, output_directory: PathLike):
+        self.output_directory = Path(output_directory)
+        self.previous_mol_2d = None
+        self.previous_mol_3d_path = None
+
+    def depict(self, id: str) -> MoleculeExport:
+        return MoleculeExport(
+            image2d=self._depict_2d(id).name,
+            image3d=self._depict_3d(id).name,
+        )
+
+    def _fetch_mol_cached(self, id, kind: Literal["2d", "3d"]) -> (Mol, Path):
+        molfile = self.output_directory.joinpath(f"{id}_{kind.upper()}.mol")
+
+        if molfile.exists():
+            mol = MolFromMolBlock(molfile.read_bytes())
+        else:
+            mol = fetch_mol(id, kind)
+            molfile.write_text(MolToMolBlock(mol))
+
+        return mol, molfile
+
+    def _depict_2d(self, id: str) -> Path:
+        mol, _ = self._fetch_mol_cached(id, kind="2d")
+        path = self.output_directory.joinpath(f"{id}_2D.png")
+
+        from rdkit.Chem.rdMolAlign import AlignMol
+
+        if self.previous_mol_2d:
+            try:
+                AlignMol(mol, self.previous_mol_2d)
+            except:
+                pass
+
+        MolToImage(mol).save(path)
+
+        self.previous_mol_2d = mol
+        return path
+
+    def _depict_3d(self, id: str) -> Path:
+        mol, molpath = self._fetch_mol_cached(id, kind="3d")
+        imagepath = self.output_directory.joinpath(f"{id}_3D.png")
+
+        with PyMOL() as p:
+            p.cmd.load(molpath.as_posix(), "this")
+            if self.previous_mol_3d_path:
+                p.cmd.load(self.previous_mol_3d_path.as_posix(), "other")
+                try:
+                    mcsalign("%this", "%other", _self=p.cmd)
+                except:
+                    pass
+                finally:
+                    self.previous_mol_3d_path = molpath
+                p.cmd.hide("%other")
+
+            p.cmd.color("gray", "(symbol C)")
+            p.cmd.show_as("sticks", "bonded")
+            p.cmd.show_as("nb_spheres", "not bonded")
+            p.cmd.hide("(symbol H)")
+            p.cmd.orient()
+            p.cmd.zoom(complete=1)
+            p.cmd.png(imagepath.as_posix(), 1000, 800, dpi=150, ray=1)
+
+        return imagepath
+
+
+def extract_mcs_as_mol(mol: Mol, other: Mol) -> Mol | None:
+    from rdkit.Chem.rdFMCS import FindMCS
+    from rdkit.Chem.rdmolfiles import MolFromSmarts
+    from rdkit.Chem.rdchem import RWMol
+
+    mcs = FindMCS([mol, other])
+    print(mcs)
+    if not mcs:
+        return None
+
+    substructure = MolFromSmarts(mcs.smartsString)
+    match = mol.GetSubstructMatch(substructure)
+    if not match:
+        return None
+
+    new_mol = RWMol(mol)
+    new_mol.BeginBatchEdit()
+    for atom in mol.GetAtoms():
+        if (id := atom.GetIdx()) not in match:
+            new_mol.RemoveAtom(id)
+    new_mol.CommitBatchEdit()
+
+    return new_mol.GetMol()
+
+
+def optimize_mol(mol: Mol) -> Mol:
+    try:
+        with BlockLogs():
+            AddHs(mol)
+            EmbedMolecule(mol)
+            MMFFOptimizeMolecule(mol)
+    except BaseException:
+        pass
+
+    return mol
+
+
+def fetch_mol(id, kind: Literal["2d", "3d"]) -> Mol:
+    if id.startswith("CHEMBL"):
+        return fetch_chembl(id, kind)
+    else:
+        return fetch_pubchem(id, kind)
+
+
+def fetch_chembl(id, kind: Literal["2d", "3d"]) -> Mol:
+    rsp = requests.get(
+        f"https://www.ebi.ac.uk/chembl/api/data/molecule/{id}?format=sdf",
+    )
+    if rsp.status_code != 200:
+        raise Exception(f"could not fetch from ChEMBL: '{id}'")
+
+    mol = MolFromMolBlock(rsp.text, removeHs=True)
+    mol.SetProp("_Name", id)
+
+    if kind == "3d":
+        return optimize_mol(mol)
+
+    return mol
+
+
+def fetch_pubchem(id, kind: Literal["2d", "3d"]) -> Mol:
+    rsp = requests.get(
+        f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/{id}/sdf?record_type={kind}"
+    )
+    if rsp.status_code != 200:
+        if kind == "3d":
+            mol = fetch_pubchem(id, kind="2d")
+            return optimize_mol(mol)
+        else:
+            raise Exception(f"could not fetch from PubChem: '{id}'")
+
+    mol = MolFromMolBlock(rsp.text, removeHs=True)
+    mol.SetProp("_Name", id)
+
+    return mol
